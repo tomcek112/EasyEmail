b725230f575e430dbc7505bca19eae2b4c17a1a3
Harry Singh <harjyot7@gmail.com>
2015-04-18 10:50:05 +0100

    Merged all into one file, still under testing

--- EmailParser.py
+++ EmailParser.py
@@ -1,4 +1,6 @@
-from flask import Flask, request
+from twilio.rest import TwilioRestClient
+from flask import Flask, request , make_response
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -7,13 +9,20 @@ def dataextract():
         from_add = request.form['from']
         sub = request.form['subject']
         text = request.form['text']
-        no_attch = request.form['attachments']
-        dataparser(to_add,no_attch,sub,text)
-        return 200
+        datparser(sub,from_add,text)
+        return make_response('',200)
+    else:
+        return "Bla"
 
-def dataparser(to_add,no_attch,sub,text):
-    twil()
-    return "LOL"
+def dataparser(sub,from_add,text):
+    twil(sub,from_add,parsed_text)
+
+
+def twil(sub,from_add,text):
+    account_sid = "ACd88394ca183bcaf1f02a2e8bcf5ebf55"
+    auth_token = "5582a52e32afa122e7492d8a506b926d"
+    client = TwilioRestClient(account_sid, auth_token)
+    message = client.messages.create(to="+4407947771680",from_="+447903530001",body="from:"+from_add+" subject:"+sub+" body:"+ text)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8000,debug=True)
