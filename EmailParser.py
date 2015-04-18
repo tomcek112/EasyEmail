8e3a5d516a1a1eb15ce2d25404f6f7f7b87abd19
Harry Singh <harjyot7@gmail.com>
2015-04-18 07:07:22 +0100

    Added a basic Email data retriever function

---
+++ EmailParser.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def dataextract():
+    if request.method =='POST':
+        from_add = request.form['from']
+        sub = request.form['subject']
+        text = request.form['text']
+        no_attch = request.form['attachments']
+        dataparser(to_add,no_attch,sub,text)
+        return 200
+
+if __name__ = '__main__':
+    app.run(debug=True)
